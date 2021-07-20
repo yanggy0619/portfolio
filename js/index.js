e802ea1fe05943f7856b65ab31b1bfc110d24666
yanggy0619 <yanggy0619@gmail.com>
2021-07-20 17:46:58 +0900

    cnt4 마크업중
    07-20_17:46

--- js/index.js
+++ js/index.js
@@ -25,5 +25,21 @@ $(document).ready(function() {
     1)aria-expanded: 아코디언 패널이 열려있어서 확장된 상태는 true, 비확장된 상태는 false
     2)aria-expanded="true" 인 경우만 aria-disabled="", true(다시클릭할 수 없는경우) / false(다시 클릭할 수 있는 경우) */
     const $acdn = $('#cnt3 .accordion')
+    // 1) header의 초기설정 아코디언 패널이 열려있는지 열려있지 않는지
+    $acdn.find('.header').attr({'aria-expanded': false});
+    // 2) 키보드제어 - 상단방향키(38), 하단방향키(40), home(36), end(35), enter/spacebar(click 이벤트가 대신함)
 
+    // 3) 마우스제어 - 클릭이벤트
+    $acdn.find('.header').on('click', function () {
+        if ($(this).hasClass('on')) {//열려진 경우 =>현재 열려진 헤더,패널 초기화
+            $(this).removeClass('on').attr({'aria-expanded': false}).removeAttr('aria-disabled').parent().next().removeClass('on').attr({tabIndex: -1});
+        } else{ // 닫긴경우
+        //아코디언 헤더
+        $(this).addClass('on').attr({'aria-expanded': true, 'aria-disabled': true}).parent().siblings('.title').children().removeClass('on').attr({'aria-expanded': false}).removeAttr('aria-disabled');
+
+        //아코디언 패널
+        $(this).parent().next().addClass('on').attr({tabIndex : 0}).siblings('.panel.on').removeClass('on').attr({tabIndex: -1});
+        }
+        
+    });
 });
